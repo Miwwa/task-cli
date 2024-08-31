dd5f4e593d3bc5d74227b1f461e3b115f27ed02b
Mikhail <eremin.ms92@gmail.com>
2024-08-31 10:33:03 +0300

    Add task status management and refactor command handling
    Introduced a new `task.go` file to define task statuses and the `Task` struct. Refactored command functions in `commands.go` to separate description updates and status updates, ensuring appropriate handling for each command type. Improved command error messages and usage instructions for better clarity.

--- commands.go
+++ commands.go
@@ -4,13 +4,13 @@ import "fmt"
 
 const helpText = `Usage: task-cli <command> [arguments]
 
-task-cli add <description>			add new task with specified <description>
-task-cli update <id> <description>  update description of task with specified <id>
-task-cli delete <id>				delete task by specified <id>
-task-cli mark-in-progress <id>		mark in-progress task by specified <id>
-task-cli mark-done <id>				mark done task specified <id>
-task-cli list						list all tasks
-task-cli list <status>				list tasks by status (done, todo, in-progress)
+task-cli add <description>              add new task with specified <description>
+task-cli update <id> <description>      update description of task with specified <id>
+task-cli delete <id>                    delete task by specified <id>
+task-cli mark-in-progress <id>          mark in-progress task by specified <id>
+task-cli mark-done <id>                 mark done task specified <id>
+task-cli list                           list all tasks
+task-cli list <status>                  list tasks by status (done, todo, in-progress)
 `
 
 type CmdError struct {
@@ -35,11 +35,15 @@ func Run(args []string) (string, error) {
 	case "add":
 		return AddCmd(args[1:])
 	case "update":
-		return UpdateCmd(args[1:])
+		return UpdateDescriptionCmd(args[1:])
 	case "delete":
+		return DeleteCmd(args[1:])
 	case "mark-in-progress":
+		return UpdateStatusCmd(args[1:], "in-progress")
 	case "mark-done":
+		return UpdateStatusCmd(args[1:], "done")
 	case "list":
+		return ListCmd(args[1:])
 	}
 	return HelpCmd()
 }
@@ -55,9 +59,38 @@ func AddCmd(args []string) (string, error) {
 	return fmt.Sprintf("Task added successfully (ID: %s)", args[0]), nil
 }
 
-func UpdateCmd(args []string) (string, error) {
+func UpdateDescriptionCmd(args []string) (string, error) {
 	if len(args) == 0 || len(args) > 2 {
 		return "", InvalidUsageError("update <id> <description>")
 	}
 	return fmt.Sprintf("Task updated successfully (ID: %s)", args[0]), nil
+}
+
+func UpdateStatusCmd(args []string, status string) (string, error) {
+	if len(args) == 0 || len(args) > 1 {
+		return "", InvalidUsageError(fmt.Sprintf("%s <id>", args[0]))
+	}
+	return fmt.Sprintf("Task updated successfully (ID: %s)", args[0]), nil
+}
+
+func DeleteCmd(args []string) (string, error) {
+	if len(args) == 0 || len(args) > 1 {
+		return "", InvalidUsageError("delete <id>")
+	}
+	return fmt.Sprintf("Task deleted successfully (ID: %s)", args[0]), nil
+}
+
+func ListCmd(args []string) (string, error) {
+	var listUsage = fmt.Sprintf("list [%s|%s|%s]", Todo, InProgress, Done)
+	if len(args) > 1 {
+		return "", InvalidUsageError(listUsage)
+	}
+	if len(args) == 0 {
+		return "All tasks", nil
+	}
+	switch args[0] {
+	case Todo, InProgress, Done:
+		return "Tasks by status", nil
+	}
+	return "", InvalidUsageError(listUsage)
 }
---
+++ task.go
@@ -0,0 +1,13 @@
+package main
+
+type TaskStatus string
+
+const (
+	Todo       = "todo"
+	InProgress = "in-progress"
+	Done       = "done"
+)
+
+type Task struct {
+	// todo
+}
